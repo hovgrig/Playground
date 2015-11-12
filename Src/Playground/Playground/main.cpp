ce7f80bd81ee3e8fca8944f56c19c2a29b9f181f
Hovhannes Grigoryan <hovgrig@users.noreply.github.com>
2015-11-12 16:35:45 +0400

    Added the names.

--- Src/Playground/Playground/main.cpp
+++ Src/Playground/Playground/main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
-    std::cout << "Hovhannes is here :)" << std::endl;
+    std::cout << "Hey there! Welcome to playground project.\n\n";
+
+    std::cout << "Few important people are missing from here:\n";
+    std::cout << "Davo\n";
+    std::cout << "Mher\n";
+    std::cout << "Seno\n";
+    std::cout << "Noro\n";
+    std::cout << "Koriun\n";
+    std::cout << "Vahag\n";
+
+    std::cout << "\nDelete your name and commit when you are in ;)";
 }
